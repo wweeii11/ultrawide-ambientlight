e9a0bcda3b1fa0be0eaa1b3cea1fe600fbfb5401
Wei Liang <weiliang@live.ca>
2025-03-10 20:05:07 -0400

    wip

--- ambientlight.cpp
+++ ambientlight.cpp
@@ -16,9 +16,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.lpszClassName = L"ambientlight";
 	RegisterClass(&wc);
 	// Create a window
-	HWND hwnd = CreateWindow(L"ambientlight", L"Ambient Light", WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, 0, 0, hInstance, 0);
+	DWORD dwExStyle = WS_EX_NOREDIRECTIONBITMAP;
+	DWORD dwStyle = WS_POPUP;
+	HWND hwnd = CreateWindowEx(dwExStyle, L"ambientlight", L"Ambient Light", dwStyle, 0, 0, 800, 600, 0, 0, hInstance, 0);
+
+	SetWindowPos(hwnd, nullptr, 0, 0, 0, 0, SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE);
+	SetWindowDisplayAffinity(hwnd, WDA_EXCLUDEFROMCAPTURE);
+
 	// Show the window
 	ShowWindow(hwnd, nCmdShow);
+
 	// Message loop
 	MSG msg = { 0 };
 	while (GetMessage(&msg, 0, 0, 0))
